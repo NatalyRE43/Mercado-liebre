b3514ab7cc2a7f8bd3bb966d4db0959e6c9631cf
NatashaRE43 <natacaudillo@gmail.com>
2022-06-02 18:48:36 -0500

    agregue linea a procfile y app

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ const app = express();
 app.use( express.static(path.resolve(__dirname, './public')));
 app.use( express.static(path.resolve(__dirname, './views')));
 
-app.listen(process.env.PORT, () => {
-        Console.log("Servidor corriendo en el puerto 3000")
+app.listen(process.env.PORT || 3000, function () {
+        Console.log("Servidor corriendo en el puerto 3000");
     }
 );
-
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
