555e644e8dd41dea76732e84e97c45fef402ebc5
NatashaRE43 <natacaudillo@gmail.com>
2022-06-02 18:52:45 -0500

    actualizando a heroku

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 app.use( express.static(path.resolve(__dirname, './public')));
 app.use( express.static(path.resolve(__dirname, './views')));
 
-app.listen(process.env.PORT || 3000, function () {
-        Console.log("Servidor corriendo en el puerto 3000");
+app.listen(process.env.PORT, () => {
+        Console.log("Servidor corriendo en el puerto 3000")
     }
 );
 
