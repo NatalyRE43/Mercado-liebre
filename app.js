76188210ff2b4fecd887abb2c9a100d994d28b36
NatashaRE43 <natacaudillo@gmail.com>
2022-06-02 18:05:12 -0500

    Intento de subida a heroku

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { Console } = require('console');
 const express = require('express');
 const path = require('path');
 
@@ -6,7 +7,8 @@ const app = express();
 app.use( express.static(path.resolve(__dirname, './public')));
 app.use( express.static(path.resolve(__dirname, './views')));
 
-app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
+app.listen(process.env.PORT || 3000, () => Console.log("Servidor corriendo en el puerto 3000"));
+
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
