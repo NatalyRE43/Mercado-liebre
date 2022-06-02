1e7eaaafa17bc6f1ebc2c98dcf30937bc2b50fe4
NatashaRE43 <natacaudillo@gmail.com>
2022-06-02 18:26:59 -0500

    Subiendo archivos con procfile

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const app = express();
 app.use( express.static(path.resolve(__dirname, './public')));
 app.use( express.static(path.resolve(__dirname, './views')));
 
-app.listen(process.env.PORT || 3000, () => Console.log("Servidor corriendo en el puerto 3000"));
+app.listen(process.env.PORT, () => {
+        Console.log("Servidor corriendo en el puerto 3000")
+    }
+);
 
 
 app.get('/', (req,res) => {
