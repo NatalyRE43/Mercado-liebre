07555d04710234810eb40aac44502ce3b1919f8e
NatashaRE43 <natacaudillo@gmail.com>
2022-06-02 19:19:28 -0500

    Agregando procfile y servidor

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = express();
 app.use( express.static(path.resolve(__dirname, './public')));
 app.use( express.static(path.resolve(__dirname, './views')));
 
-app.listen(3000, () => console.log());
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Servidor corriendo');
+});
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
